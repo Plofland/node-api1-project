6faf01ace9bf23af1fb82f33d91ccfeab139a162
Plofland <ploflan@gmail.com>
2021-02-08 18:55:56 -0500

    Fixed postman bug and am able to view users array

--- api/server.js
+++ api/server.js
@@ -1,3 +1,65 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express');
+const server = express();
+const dbFunctions = require('./users/model');
+
+console.log('Server working');
+
+server.use(express.json());
+
+server.get('/users', (req, res) => {
+  dbFunctions
+    .find()
+    .then((users) => res.status(200).json(users))
+    .catch((error) => res.status(500).json({ error: error.message }));
+});
+
+server.use('*', (req, res) => {
+  res.status(404).json({ message: '404 Not Found, sorry mate' });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+// //POST a new user to the user array
+// server.post('/users', (req, res) => {
+//   const { name, role } = req.body;
+//   if (!name || !role) {
+//     res.status(400).json({ message: 'Name and role are required' });
+//   } else {
+//     const newUser = { id: generate(), name, role };
+//     users.push(newUser);
+//     res.status(200).json(newUser);
+//   }
+// });
+
+// //GET the array of users
+// server.get('/users', (req, res) => {
+//   res.status(200).json(users);
+// });
+
+// //GET an individual user with this id
+// server.get('/users/:id', (req, res) => {
+//   const idVar = req.params.id;
+//   const user = users.find((usr) => usr.id === idVar);
+//   if(!user){
+//     res.status(404).json({message: `ID: ${idVar} does not exist`})
+//   } else{
+//     res.status(200).json(user);
+//   }
+//   //something goes here?
+// });
+
+// //DELETE user with this id
+// server.delete('/users/:id', (req, res) => {
+//   res.status(status).json({ message: 'successMessage' });
+// });
+
+// //PUT update the user with this id
+// server.put('/users/:id', (req, res) => {
+//   res.status(status).json({ message: 'successMessage' });
+// });
+
+// server.use('*', (req, res) => {
+//   res.status(404).json({ message: '404 Not Found, sorry mate' });
+// });
--- index.js
+++ index.js
@@ -1,46 +1,5 @@
 const server = require('./api/server');
 
-const port = 5000;
+const PORT = 5000;
 
-// START YOUR SERVER HERE
-
-const express = require('express');
-const generate = require('shortid').generate;
-
-const app = express();
-app.use(express.json());
-
-let users = [
-  { id: generate(), name: 'Pathfinder', role: 'Support/Scout' },
-  { id: generate(), name: 'Octane', role: 'Forward/Offence' },
-  { id: generate(), name: 'Wraith', role: 'Forward/Offence' },
-  { id: generate(), name: 'Wattson', role: 'Support/Defence' },
-  { id: generate(), name: 'Crypto', role: 'Scout' }
-];
-
-app.post('/api/users', (req, res) => {
-  const { name, role } = req.body;
-  if (!name || !role) {
-    res.status(400).json({ message: 'Name and role are required' });
-  } else {
-    const newUser = { id: generate(), name, role };
-    users.push(newUser);
-    res.status(200).json(newUser);
-  }
-});
-
-app.get('/api/users', (req, res) => {
-  res.status(200).json(users);
-});
-
-app.get('/api/users/:id', (req, res) => {
-  res.status(status).json({ message: 'successMessage' });
-});
-
-app.delete('/api/users/:id', (req, res) => {
-  res.status(status).json({ message: 'successMessage' });
-});
-
-app.put('/api/users/:id', (req, res) => {
-  res.status(status).json({ message: 'successMessage' });
-});
+server.listen(PORT, () => console.log(`Running on port: ${PORT}`));
