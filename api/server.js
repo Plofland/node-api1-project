834fe16fae31890d0b9e246cd6f7836066c726d7
Plofland <ploflan@gmail.com>
2021-02-08 22:29:03 -0500

    PUT request to update user is functional

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express');
+const { restart } = require('nodemon');
 const server = express();
 const dbFunctions = require('./users/model');
 
@@ -59,6 +60,31 @@ server.delete('/users/:id', (req, res) => {
       }
     })
     .catch((error) => res.status(500).json({ message: error.message }));
+});
+
+server.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  if (!changes.name || !changes.bio) {
+    res
+      .status(400)
+      .json({ message: 'Please provide name and bio for the user' });
+  } else {
+    try {
+      const updatedUser = await dbFunctions.update(id, changes);
+      if (updatedUser) {
+        res.status(200).json(updatedUser);
+      } else {
+        res
+          .status(404)
+          .json({ message: 'The user with the specified ID does not exist' });
+      }
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: 'The user information could not be modified' });
+    }
+  }
 });
 
 server.use('*', (req, res) => {
